dfeccaa861501867db94fcada09b093ca119b2a3
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-29 15:20:44 +0700

    finish: step-4 app-layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import './App.scss';
 function App() {
-    return <div>React-App</div>;
+    return (
+        <div className='todo'>
+            <div className='todo__header'>Header</div>
+            <div className='todo__sidebar'>SideBar</div>
+            <div classNAme='todo__content'>TodoContent</div>
+        </div>
+    );
 }
 
 export default App;
