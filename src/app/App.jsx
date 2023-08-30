03eb68271597daccc7659aa0c87982910b104286
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-30 11:53:52 +0700

    finish:list UI

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 // Dependencies
 import './App.scss';
-import { FaHome } from 'react-icons/fa';
+import { FaHome, FaInbox, FaCalendar, FaCalendarAlt } from 'react-icons/fa';
 import Header from '../components/Header';
 
 function App() {
@@ -9,7 +9,27 @@ function App() {
             <div className='todo__header'>
                 <Header />
             </div>
-            <div className='todo__sidebar'>SideBar</div>
+            <div className='todo__sidebar'>
+                <aside className='sidebar'>
+                    <section className='sidebar__category'>
+                        <ul className='list'>
+                            <li className='list__item'>
+                                <FaInbox className='list__item__icon' />
+                                <p className='list__item__text'>Inbox</p>
+                            </li>
+                            <li className='list__item'>
+                                <FaCalendar className='list__item__icon' />
+                                <p className='list__item__text'>Today</p>
+                            </li>
+                            <li className='list__item'>
+                                <FaCalendarAlt className='list__item__icon' />
+                                <p className='list__item__text'>Next 7 days</p>
+                            </li>
+                        </ul>
+                    </section>
+                    <section className='sidebar__category'>2</section>
+                </aside>
+            </div>
             <div className='todo__content'>TodoContent</div>
         </div>
     );
