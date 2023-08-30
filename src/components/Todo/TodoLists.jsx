e5306e899670a35d7085b0ae78c13a9d99438c8e
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-31 00:45:27 +0700

    finish:todolist css

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,11 +1,14 @@
 import styles from './TodoLists.module.scss';
-import { FaTrashAlt, FaPen, FaRegCircle } from 'react-icons/fa';
+import { FaTrashAlt, FaPen } from 'react-icons/fa';
+import { HiOutlineCheck } from 'react-icons/hi';
 
 function TodoLists() {
   return (
     <ul className={styles.todo__lists}>
       <li className={styles.todo}>
-        <span className={styles.todo__checkbox}>{/* <FaRegCircle /> */}</span>
+        <div className={styles.todo__checkbox}>
+          <HiOutlineCheck className={styles.todo__checkbox__icon} />
+        </div>
         <p className={styles.todo__task}>todo-item 1 </p>
         <span className={styles.todo__date}>30 Aug</span>
         <div className={styles.todo__action}>
