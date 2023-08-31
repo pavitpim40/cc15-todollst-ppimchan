bc43daa9bb1807d6a77bf73fbfda5d62ef2331b4
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-31 16:22:00 +0700

    refactor:TodoList to todoItem

---
+++ src/components/Todo/TodoItem.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { FaTrashAlt, FaPen } from 'react-icons/fa';
+import { HiOutlineCheck } from 'react-icons/hi';
+
+import TodoForm from './TodoForm';
+import styles from './TodoItem.module.scss';
+
+function TodoItem() {
+  const [isOpenForm, setIsOpenForm] = useState(false);
+
+  const handleClick = function () {
+    setIsOpenForm(!isOpenForm);
+  };
+  return (
+    <>
+      {isOpenForm ? (
+        <TodoForm textSubmit='Edit Task' setIsOpenForm={setIsOpenForm} />
+      ) : (
+        <li className={styles.todo}>
+          <div className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}>
+            <HiOutlineCheck className={styles.todo__checkbox__icon} />
+          </div>
+          <p className={`${styles.todo__task} ${styles.todo__task__done}`}>todo-item 1 </p>
+          <span className={styles.todo__date}>30 Aug</span>
+          <div className={styles.todo__action}>
+            <span onClick={handleClick}>
+              <FaPen className={styles.todo__edit} />
+            </span>
+            <span>
+              <FaTrashAlt className={styles.todo__delete} />
+            </span>
+          </div>
+        </li>
+      )}
+    </>
+  );
+}
+
+export default TodoItem;
--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,38 +1,13 @@
-import { useState } from 'react';
-import { FaTrashAlt, FaPen } from 'react-icons/fa';
-import { HiOutlineCheck } from 'react-icons/hi';
-
-import TodoForm from './TodoForm';
+import TodoItem from './TodoItem';
 import styles from './TodoLists.module.scss';
 
 function TodoLists() {
-  const [isOpenForm, setIsOpenForm] = useState(false);
-
-  const handleClick = function () {
-    setIsOpenForm(!isOpenForm);
-  };
   return (
     <ul className={styles.todo__lists}>
-      {/* 1 TodoItem */}
-      {isOpenForm ? (
-        <TodoForm textSubmit='Edit Task' setIsOpenForm={setIsOpenForm} />
-      ) : (
-        <li className={styles.todo}>
-          <div className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}>
-            <HiOutlineCheck className={styles.todo__checkbox__icon} />
-          </div>
-          <p className={`${styles.todo__task} ${styles.todo__task__done}`}>todo-item 1 </p>
-          <span className={styles.todo__date}>30 Aug</span>
-          <div className={styles.todo__action}>
-            <span onClick={handleClick}>
-              <FaPen className={styles.todo__edit} />
-            </span>
-            <span>
-              <FaTrashAlt className={styles.todo__delete} />
-            </span>
-          </div>
-        </li>
-      )}
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </ul>
   );
 }
