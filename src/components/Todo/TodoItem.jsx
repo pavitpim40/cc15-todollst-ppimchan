e8467c57a85fe1f2d7c56d48a4106e03b72cb645
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-31 16:48:31 +0700

    pass props from todoList to todoItem

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -5,7 +5,11 @@ import { HiOutlineCheck } from 'react-icons/hi';
 import TodoForm from './TodoForm';
 import styles from './TodoItem.module.scss';
 
-function TodoItem() {
+// function TodoItem(props) {
+// Object Destructuring (Props)
+// const { task, done, date } = props;
+
+function TodoItem({ task, done, date }) {
   const [isOpenForm, setIsOpenForm] = useState(false);
 
   const handleClick = function () {
@@ -17,11 +21,11 @@ function TodoItem() {
         <TodoForm textSubmit='Edit Task' setIsOpenForm={setIsOpenForm} />
       ) : (
         <li className={styles.todo}>
-          <div className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}>
+          <div className={`${styles.todo__checkbox}  ${done ? styles.todo__checkbox__done : ''}`}>
             <HiOutlineCheck className={styles.todo__checkbox__icon} />
           </div>
-          <p className={`${styles.todo__task} ${styles.todo__task__done}`}>todo-item 1 </p>
-          <span className={styles.todo__date}>30 Aug</span>
+          <p className={`${styles.todo__task} ${done ? styles.todo__task__done : ''}`}>{task}</p>
+          <span className={styles.todo__date}>{date}</span>
           <div className={styles.todo__action}>
             <span onClick={handleClick}>
               <FaPen className={styles.todo__edit} />
--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -4,10 +4,8 @@ import styles from './TodoLists.module.scss';
 function TodoLists() {
   return (
     <ul className={styles.todo__lists}>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoItem task='DoHW' done={true} date='31 Aug' />
+      <TodoItem task='Drink' done={false} date='1 Sep' />
     </ul>
   );
 }
