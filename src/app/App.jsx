19de2cdb06da79a806a1be2d5397a257d1d435f3
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-07 11:48:52 +0700

    update code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 // Dependencies
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import dayjs from 'dayjs';
 import './App.scss';
@@ -9,34 +9,72 @@ import TodoHeader from '../components/Todo/TodoHeader';
 import TodoCreate from '../components/Todo/TodoCreate';
 import TodoLists from '../components/Todo/TodoLists';
 
-const data = [
-  { id: nanoid(), task: 'Suspendisse potenti.', status: false, due_date: '2023-04-26' },
-  {
-    id: nanoid(),
-    task: 'In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.',
-    status: false,
-    due_date: '2023-05-08',
-  },
-  {
-    id: nanoid(),
-    task: 'Aenean fermentum. Donec ut mauris eget massa tempor convallis.',
-    status: false,
-    due_date: '2023-04-30',
-  },
-];
+// const data = [
+//   { id: nanoid(), task: 'Suspendisse potenti.', status: false, due_date: '2023-04-26' },
+//   {
+//     id: nanoid(),
+//     task: 'In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.',
+//     status: false,
+//     due_date: '2023-05-08',
+//   },
+//   {
+//     id: nanoid(),
+//     task: 'Aenean fermentum. Donec ut mauris eget massa tempor convallis.',
+//     status: false,
+//     due_date: '2023-04-30',
+//   },
+// ];
+
+const END_POINT = 'http://localhost:8080/api/todos';
 
 function App() {
-  const [allTodos, setAllTodos] = useState(data);
+  const [allTodos, setAllTodos] = useState([]);
+
+  useEffect(() => {
+    // fetchAllTodo
+    async function fetchAllTodo() {
+      try {
+        let response = await fetch('http://localhost:8080/api/todos', { method: 'GET' });
+        let todoData = await response.json();
+        console.log(todoData);
+        setAllTodos(todoData.todos);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchAllTodo();
+  }, []);
+
+  console.log('render');
 
   // add : CreateTodo
-  const addTodo = function (taskName) {
+  const addTodo = async function (taskName) {
     const newTodo = {
-      id: nanoid(),
       task: taskName,
       status: false,
       due_date: dayjs().format('YYYY-MM-DD'),
     };
-    setAllTodos((p) => [newTodo, ...p]);
+
+    try {
+      // SEND REQUEST : POST
+      // WAIT RESPONSE
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(newTodo),
+      };
+      let response = await fetch(END_POINT, options);
+      let data = await response.json();
+      console.log(data);
+
+      // Update STATE
+      setAllTodos((p) => [data.todo, ...p]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // delete : DeleteTodo
